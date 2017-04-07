f755d6b9db137217670c9dc72c1cf3d47f1df192
Wisn <wuyishun_kmk@outlook.com>
2017-04-07 10:45:51 +0800

    SnackbarUtils
    SnackbarUtils

--- PMClient/lib_pm/src/main/java/com/wisn/pmlib/activity/tips/SnackbarActivity.java
+++ PMClient/lib_pm/src/main/java/com/wisn/pmlib/activity/tips/SnackbarActivity.java
@@ -1,6 +1,8 @@
 package com.wisn.pmlib.activity.tips;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +10,7 @@ import android.widget.Toast;
 
 import com.wisn.pmlib.R;
 import com.wisn.pmlib.activity.base.BaseActivity;
+import com.wisn.pmlib.utils.SnackbarUtils;
 
 /**
  * Created by wisn on 2017/4/1.
@@ -40,12 +43,32 @@ public class SnackbarActivity extends BaseActivity implements View.OnClickListen
             Snackbar.make(mSnackbar_top,"这是massage", Snackbar.LENGTH_LONG).setAction("这是action", new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(SnackbarActivity.this, "你点击了action", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(SnackbarActivity.this, "你点击了action", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(mSnackbar_top,"action", Snackbar.LENGTH_LONG).show();
                 }
             }).setActionTextColor(getResources().getColor(R.color.white)).show();
 
         }else if(v==snackbar_custom_view){
-
+            Snackbar snackbar= SnackbarUtils.ShortSnackbar(mSnackbar_top, "妹子删了你发出的消息", SnackbarUtils.Warning).setActionTextColor(
+                    Color.RED).setAction("再次发送", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(SnackbarActivity.this,"dddd",Toast.LENGTH_SHORT).show();
+                            SnackbarUtils.LongSnackbar(mSnackbar_top,"妹子已将你拉黑",SnackbarUtils.blue).setActionTextColor(Color.WHITE).show();
+                        }
+                    },2000);
+                }
+            });
+            snackbar.show();
+//
+//            SnackbarUtils.SnackbarAddView(snackbar,R.layout.snackbar_addview,0);
+//
+//            SnackbarUtils.SnackbarAddView(snackbar,R.layout.snackbar_addview2,2);
+//
+//            snackbar.show();
         }
     }
 }
--- PMClient/lib_pm/src/main/java/com/wisn/pmlib/utils/SnackbarUtils.java
+++ PMClient/lib_pm/src/main/java/com/wisn/pmlib/utils/SnackbarUtils.java
@@ -1,12 +1,10 @@
 package com.wisn.pmlib.utils;
 
 import android.graphics.Color;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.Snackbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
