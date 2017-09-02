546253425456b87c4cd6a35d66089def7a0e9df7
Wisn <wuyishun_kmk@outlook.com>
2017-09-02 22:39:52 +0800

    add svg animation

--- PMClient/lib_pm/src/main/java/com/wisn/pmlib/activity/svg/SvgActivity.java
+++ PMClient/lib_pm/src/main/java/com/wisn/pmlib/activity/svg/SvgActivity.java
@@ -1,19 +1,26 @@
 package com.wisn.pmlib.activity.svg;
 
+import android.graphics.drawable.Animatable;
 import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.wisn.pmlib.R;
 
 public class SvgActivity extends AppCompatActivity {
-
+    // 针对低版本的低于21版本的
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
     private ImageView mImageView1;
     private ImageView mImageView2;
+    private ImageView mImageView3;
     private boolean hearisStart=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,7 @@ public class SvgActivity extends AppCompatActivity {
         setContentView(R.layout.activity_svg);
         mImageView1 = (ImageView) findViewById(R.id.svgImage1);
         mImageView2 = (ImageView) findViewById(R.id.svgImage2);
+        mImageView3 = (ImageView) findViewById(R.id.svgImage3);
         mImageView1.setImageResource(R.drawable.ic_camera_roll_black_24dp);
         mImageView2.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -34,6 +42,15 @@ public class SvgActivity extends AppCompatActivity {
                     target.start();
                 }
                 hearisStart=!hearisStart;
+            }
+        });
+        mImageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = mImageView3.getDrawable();
+                if(drawable instanceof Animatable){
+                    ((Animatable)drawable).start();
+                }
             }
         });
     }
